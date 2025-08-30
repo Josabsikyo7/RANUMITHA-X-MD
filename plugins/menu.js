ddc1bc55a20a873696ccd8cb5e7d7ec2265d4fba
Josabsikyo7 <sokiyakoilokeyamkiyo@gmail.com>
2025-08-30 21:29:04 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,16 +1,71 @@
 const config = require('../config');
-const { cmd } = require('../command');
+const { cmd, commands } = require('../command');
+const { runtime } = require('../lib/functions');
+const axios = require('axios');
+const os = require("os");
+
+// Fake ChatGPT vCard
+const fakevCard = {
+    key: {
+        fromMe: false,
+        participant: "0@s.whatsapp.net",
+        remoteJid: "status@broadcast"
+    },
+    message: {
+        contactMessage: {
+            displayName: "© Mr Hiruka",
+            vcard: `BEGIN:VCARD
+VERSION:3.0
+FN:Meta
+ORG:META AI;
+TEL;type=CELL;type=VOICE;waid=13135550002:+13135550002
+END:VCARD`
+        }
+    }
+};
 
 cmd({
-    pattern: "test",
-    alias: ["test1"],
-    react: "🇱🇰",
-    desc: "my cmd",
-    category: "download",
+    pattern: "menu",
+    alise: ["getmenu","list","ranulist","ranumenu"],
+    desc: "Show interactive menu system",
+    category: "menu",
+    react: "📂",
     filename: __filename
-}, async (conn, m, mek, { from, q, reply }) => {
+}, async (conn, mek, m, { from, pushname, reply }) => {
     try {
-        const info = `Hello yaluwe`;
+    
+            // Count total commands
+        const totalCommands = Object.keys(commands).length;
+        
+        const info = `👋 *𝘏𝘌𝘓𝘓𝘖𝘞* ${pushname} 
+
+ 🎀 𝗪elcome to RANUMITHA-X-MD🎗️
+
+*╭──「 MENU 」*
+*│*🐼 *\`Bot\`*: *𝐑𝐀𝐍𝐔𝐌𝐈𝐓𝐇𝐀-𝐗-𝐌𝐃*
+*│*👤 *\`User\`*: ${pushname}
+*│*👨‍💻 *\`Owner\`*: *ᴴᴵᴿᵁᴷᴬ ᴿᴬᴺᵁᴹᴵᵀᴴᴬ*
+*│*⏰ *\`Uptime\`*: ${runtime(process.uptime())}
+*│*⏳ *\`Ram\`*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
+*│*🫟 *\`Version\`*: ${config.BOT_VERSION}
+*│*🪙 *\`Commands\`*: ${totalCommands}
+*│*🖊️ *\`Prefix\`*: ${config.PREFIX}
+╰───────────────●●►
+
+*1. │  🤵‍♂ -* Owner Menu
+*2. │  🤖 -* Ai Menu
+*3. │  🔍 -* Search Menu
+*4. │  📥 -* Download Menu
+*5. │  😁 -* Fun Menu
+*6. │  📂 -* Main Menu
+*7. │  🔄 -* Convert Menu
+*8. │  📌 -* Other Menu
+*9. │  🎨 -* Logo Menu
+*10.│ 🖼️ -* Imagine Menu
+*11.│ 👥 -* Group Menu
+*12.│ ⚙️ -* Setting Menu
+
+> © Powerd by 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗 🌛`;
         const image = "https://raw.githubusercontent.com/Ranumithaofc/RANU-FILE-S-/refs/heads/main/images/IMG-20250711-WA0010.jpg"; // define image url
         const audioUrl = "https://github.com/Ranumithaofc/RANU-FILE-S-/raw/refs/heads/main/Audio/menujs-audio.mp3"; // audio url / local file
 
@@ -51,8 +106,28 @@ cmd({
                     await conn.sendMessage(from, { text: "Hodai" }, { quoted: mek });
                 } else if (userReply === "2") {
                     await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
+                } else if (userReply === "3") {
+                    await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
+                } else if (userReply === "4") {
+                    await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
+                } else if (userReply === "5") {
+                    await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
+                } else if (userReply === "6") {
+                    await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
+                } else if (userReply === "7") {
+                    await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
+                } else if (userReply === "8") {
+                    await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
+                } else if (userReply === "9") {
+                    await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
+                } else if (userReply === "10") {
+                    await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
+                } else if (userReply === "11") {
+                    await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
+                } else if (userReply === "12") {
+                    await conn.sendMessage(from, { text: "aulak na" }, { quoted: mek });
                 } else {
-                    return await reply("❌ Invalid choice! Reply with 1 or 2");
+                    return await reply("❌ Invalid choice! Reply with 1 or 12");
                 }
             } catch (error) {
                 console.error(error);
